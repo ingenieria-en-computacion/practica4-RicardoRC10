51adebd61c71c4e842ca4d1c0623a917e1956314
Rios Cruz Ricardo <balenrivers3@gmail.com>
2025-03-06 10:40:13 -0600

    ejercicios

--- ejercicios/ejercicio1.c
+++ ejercicios/ejercicio1.c
@@ -4,9 +4,10 @@
 int main() {
     int n;
     printf("Ingrese el tamaño del arreglo: ");
-    
+    scanf("%i", &n);
 
     int *arr;
+    arr= (int*)malloc(n *sizeof(int));
     if (arr == NULL) {
         printf("Error: No se pudo asignar memoria.\n");
         return 1;
@@ -14,15 +15,15 @@ int main() {
 
     printf("Ingrese %d valores:\n", n);
     for (int i = 0; i < n; i++) {
-        
+        scanf("%p", (arr + i));
     }
 
     printf("El arreglo es: ");
     for (int i = 0; i < n; i++) {
-        
+        printf ("%d \t", *(arr + i));
     }
     printf("\n");
 
-    
+    free(arr);
     return 0;
 }
--- ejercicios/ejercicio2.c
+++ ejercicios/ejercicio2.c
@@ -4,9 +4,9 @@
 int main() {
     int m, n;
     printf("Ingrese el número de filas: ");
-    
+    scanf("%i", &m);
     printf("Ingrese el número de columnas: ");
-    
+    scanf("%i", &n);
 
     int *matrix = (int *)calloc(m * n, sizeof(int));
     if (matrix == NULL) {
@@ -17,7 +17,13 @@ int main() {
     printf("Matriz inicializada con ceros:\n");
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            
+            scanf("%p\t", (matrix + i + j));
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d\t", *(matrix + i + j));
         }
         printf("\n");
     }
@@ -25,12 +31,18 @@ int main() {
     printf("Ingrese las coordenadas (fila, columna) y el valor para modificar (ej. 1 2 5):\n");
     int row, col, value;
     //lee los varlores solicitados
-    matrix[(row-1) * n + (col-1)] = value;
+    printf("Ingrese el número de filas: ");
+    scanf("%i", &row);
+    printf("Ingrese el número de columnas: ");
+    scanf("%i", &col);
+    printf("Ingrese el valor ");
+    scanf("%i", &value);
+    matrix[(row-1) + (col-1)] = value;
 
     printf("Matriz actualizada:\n");
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            
+            printf("%d\t", *(matrix + i + j));
         }
         printf("\n");
     }
